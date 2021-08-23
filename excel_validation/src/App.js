ee21afaeaf0a10c1561798337d55fca63dd2f1f2
vishumane <47922687+vishumane@users.noreply.github.com>
2021-08-23 18:35:39 +0530

    search filter according to user choice
    search filter according to user choice

--- excel_validation/src/App.js
+++ excel_validation/src/App.js
@@ -1,9 +1,11 @@
 import React from'react';
  import ExcelValidation from'./components/ExcelValidation';
+ import SearchFilter from'./components/Searchfilter/SearchFilter';
 const App=()=> {
   return (
     <div className="App">
       <ExcelValidation/>
+      <SearchFilter/>
     </div>
   );
 }
---
+++ excel_validation/src/components/Searchfilter/SearchFilter.jsx
@@ -0,0 +1,63 @@
+import React,{useState,useEffect}from 'react';
+import DataTable from'./DataTable';
+
+ const SearchFilter=()=> {
+     const [data,setData]=useState([]);
+     const[q,setQ]=useState([]);
+     const [searchcoloumns,setSearchColoumns]=useState(["title","id"])
+
+     useEffect( ()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos/')
+         .then(response => response.json())
+            .then(json => setData(json));
+     },[]);
+     
+    //  console.log(q);
+const search=(rows)=>{
+    // return rows.filter(row=>row.title.toLowerCase().indexOf(q)>-1)
+    return rows.filter((row)=>
+    searchcoloumns.some(
+        (column)=>
+        row[column].toString().toLowerCase().indexOf(q) > -1
+
+       )
+    );
+}
+
+const columns=data[0]&& Object.keys(data[0]);
+    return (
+        <div>
+            <p>*****searching according to user choice****</p>
+            <input type="text" value={q} onChange={(e)=>setQ(e.target.value)}></input>
+            {/* <DataTable data={data}/></DataTable> */}
+            {columns&& columns.map((column)=><label>
+
+                <input type="checkbox"
+                checked={searchcoloumns.includes(column)}
+                onChange={(e)=>{
+                    const checked=searchcoloumns.includes(column);
+                    setSearchColoumns((prev)=>
+                    checked
+                    ?
+                    prev.filter((sc)=>sc!==column)
+                    :[...prev,column]
+                    );
+                }}
+                ></input>
+                {column}
+            </label>
+            )}
+            <DataTable 
+            data={search(data)}
+            />
+        </div>
+    )
+}
+export default SearchFilter;
+
+
+
+
+
+
+
