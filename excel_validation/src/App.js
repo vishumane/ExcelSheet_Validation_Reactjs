86a91b463c73a8a38519e532f87293c11ae27d4b
vishumane <47922687+vishumane@users.noreply.github.com>
2021-08-23 19:03:47 +0530

    OTP Box structure for verify
    OTP Box structure for verify

--- excel_validation/src/App.js
+++ excel_validation/src/App.js
@@ -1,11 +1,13 @@
 import React from'react';
  import ExcelValidation from'./components/ExcelValidation';
  import SearchFilter from'./components/Searchfilter/SearchFilter';
+ import Otpverifcation from'./components/OTPVerify/Otpverifcation';
 const App=()=> {
   return (
     <div className="App">
       <ExcelValidation/>
       <SearchFilter/>
+      <Otpverifcation/>
     </div>
   );
 }
